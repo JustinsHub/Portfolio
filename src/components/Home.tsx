f8aba865ee1eb91619fd7b307584281b9df8e8a6
Justin <contactjustinzamora@gmail.com>
2022-05-19 12:22:27 -0700

    Implemented MUI grid and moved navbar and home components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,35 @@
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import Navbar from "./Navbar";
 
 const Home: React.FC = () => {
+
+    //box each component 25rem height background white grid 6/6
+    //Typeography on home
     return (
         <section>
-            Home
+            <Navbar/>
+            
+            <Box
+             sx={{
+                width: "auto",
+                height: "25rem",
+                backgroundColor: 'lightcoral'
+              }}>
+
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6} lg={6}>
+
+                    <Typography sx={{fontSize: "2rem", fontWeight: "800"}}>Justin Zamora</Typography>
+                    Software Engineer
+                </Grid>
+
+                <Grid item xs={0} md={6} lg={6}>
+                    Profile Picture
+                </Grid>
+            </Grid>
+            </Box>
         </section>
     )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Home from './Home';
 import About from './About';
 import Skills from './Skills';
@@ -11,17 +11,33 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { styled } from "@mui/system";
+import { Stack } from "@mui/material";
 
 const NavbarTypography = styled(Typography)({
     cursor: "pointer",
-    margin: "1.3rem"
+    margin: "1.3rem",
 })
 
 
+interface RefObject {
+    scrollIntoView: () => void
+  }
 const Navbar:React.FC = () => {
+    const myAbout= useRef<RefObject>(null)
+    const myProjects = useRef(null)
+    const mySkills = useRef(null)
+    const myContact = useRef(null)
+    
+    // const scrollAbout = () => myAbout.current.scrollIntoView({behavior: "smooth"}) 
+    // const scrollProjects = () => myProjects.current.scrollIntoView({behavior: "smooth"}) 
+    // const scrollSkills = () => mySkills.current.scrollIntoView({behavior: "smooth"}) 
+    // const scrollContact = () => myContact.current.scrollIntoView({behavior: "smooth"}) 
+
+
+    //Have these passed in navbar and redirect to each component
   return (
     <Box>
-      <AppBar position="static" color="inherit">
+      {/* <AppBar position="static" color="inherit"> */}
         <Toolbar
         sx={{
             justifyContent: "center"
@@ -33,7 +49,13 @@ const Navbar:React.FC = () => {
           <NavbarTypography>Projects</NavbarTypography>
           <NavbarTypography>Contact</NavbarTypography>
         </Toolbar>
-      </AppBar>
+      {/* </AppBar> */}
+
+    {/* <Stack direction="row" justifyContent="space-between" spacing={2}>
+      <Box bgcolor="dodgerblue" flex={1} p={2}>Hello</Box>
+      <Box bgcolor="peru" flex={1}>World</Box>
+      <Box bgcolor="orchid" flex={10}>Now</Box>
+    </Stack> */}
     </Box>
   );
 }
