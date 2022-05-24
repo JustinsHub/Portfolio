e109fd5c9e7aecfd9033bd743033c2fb4b7bdd08
Justin <contactjustinzamora@gmail.com>
2022-05-24 00:30:54 -0700

    UI implemented to skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,41 @@
-import { Grid } from "@mui/material";
-import { Box } from "@mui/system";
 import React from "react";
+import { Button, Grid, List, Paper, Stack, Typography } from "@mui/material";
+import { Box, styled } from "@mui/system";
 import { GlobalBoxWrap, GlobalSubTitle } from "../styles/globalMUI";
 import SkillsImage from '../styles/images/SkillsImage.png'
+
+const PaperSkills = styled(Paper)({
+    backgroundColor: "dodgerblue"
+})
+
+const ourSkills = ['JavaScript', 'TypeScript', 'Python','HTML', 'CSS', 'React', 'Node', 'Express', 'Flask', 'PostgreSQL', 'Prisma', 'Bootstrap', 'Git' ]
 
 const Skills: React.FC = () => {
     return (
         <section>
             <GlobalBoxWrap>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={6} lg={6} sx={{display: "flex", justifyContent: "center"}}>
+                    <Grid item xs={12} md={6} lg={6}>
+                        <Box sx={{textAlign: "center"}}>
                         <GlobalSubTitle>Skills</GlobalSubTitle>
+                                {ourSkills.map((skill) => 
+                                    <Button variant="outlined" sx={{
+                                        margin: "1rem", 
+                                        color: "black", 
+                                        borderColor: "#dda73b",
+                                        textTransform: "none",
+                                        '&:hover': {
+                                            backgroundColor: "rgba(221,167,59,0.2)",
+                                            borderColor: "#dda73b"
+                                        }
+                                        }}>
+                                        {skill} 
+                                    </Button>
+                                )}
+                        </Box>
                     </Grid>
                     <Grid item xs={12} md={6} lg={6} sx={{display: "flex", justifyContent: "center"}}>
-                       <Box component="img" alt="about me picture" src={SkillsImage}></Box>
+                       <Box component="img" alt="portfolio skills" src={SkillsImage} sx={{ maxWidth: 'md' }}></Box>
                     </Grid>
                 </Grid>
             </GlobalBoxWrap>
