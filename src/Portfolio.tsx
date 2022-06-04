316f8476f984d16a4127f1842943a1f4892b50b8
Justin <contactjustinzamora@gmail.com>
2022-06-04 12:31:29 -0700

    updated readme

--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -3,8 +3,6 @@ import Home from './components/Home';
 
 const Portfolio: React.FC = () => {
 
-  //Active on navbar, scroll on navbar on click, grid correction/mobile responsive
-
   return (
     <section>
         <Home/>
